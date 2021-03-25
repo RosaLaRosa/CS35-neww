c5c4a75cdbae7fe5a6e573eacdb532c888e81727
RosaLaRosa <77786217+RosaLaRosa@users.noreply.github.com>
2021-03-25 13:31:07 +0100

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,15 @@
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=217.22.213.11"
+export CATALINA_OPTS="$CATALINA_OPTS -
+Dcom.sun.management.jmxremote.rmi.port=12345"
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true"
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote"
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=12345"
+export CATALINA_OPTS="$CATALINA_OPTS -
+Dcom.sun.management.jmxremote.authenticate=false"
+export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+export CATALINA_OPTS="$CATALINA_OPTS –Xms6144m –Xmx6144m -XX:+UseConcMarkSweepGC"
+export CATALINA_OPTS="$CATALINA_OPTS -
+Denvironment.configuration.path=/opt/tomcat/conf"
+export CATALINA_OPTS="$CATALINA_OPTS -Denvironment.configuration.name=webmeeting"
+export CATALINA_OPTS="$CATALINA_OPTS -
+Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
